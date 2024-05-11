9599d13c6132b208fb3001304401021d05fd62ce
pump9918 <hoyeon9918@gmail.com>
2024-05-11 13:00:52 +0900

    Chore : 서버 DB 확인을 위한 테스트 모델 생성

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    testname = models.CharField(max_length=255)
