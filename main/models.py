eebb638c6fc7f2f9f5fab11d500aa9131e5b6a69
pump9918 <hoyeon9918@gmail.com>
2024-05-11 13:30:24 +0900

    Chore : test db 속성 추가

--- main/models.py
+++ main/models.py
@@ -5,3 +5,4 @@ class Test(models.Model):
     id = models.BigAutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     testname = models.CharField(max_length=255)
+    testdescription = models.CharField(max_length=255)
