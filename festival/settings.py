3c7703b454836362eee1b14c5b4d906d5a2f0616
pump9918 <hoyeon9918@gmail.com>
2024-05-11 11:20:01 +0900

    chore : settings 주석 삭제

--- festival/settings.py
+++ festival/settings.py
@@ -103,15 +103,6 @@ if config('DJANGO_DEPLOY', default=False, cast=bool):
     }
 
 
-
-# # 기본 설정 (개발환경)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
